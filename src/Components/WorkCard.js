99be18109a338d7179c93707b06e5c941217160c
code-with-ayon <ayonch7@gmail.com>
2023-03-07 21:40:35 +0530

    1st commit

--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -1,38 +1,55 @@
 import "./WorkCardStyle.css";
 
 import React from "react";
-import pro1 from "../assets/project1.png";
+import pro1 from "../assets/trippy.png";
+import pro2 from "../assets/port.png";
 import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   return (
     <div className="work-container">
       <h1 className="project-heading">Projects</h1>
+       <center>
       <div className="project-container">
         <div className="project-card">
           <img src={pro1} alt="image"></img>
-          <h2 className="pro-title">Ecommerce Projects</h2>
+          <h2 className="pro-title">Tour and Travel Projects</h2>
           <div className="pro-details">
             <p>
-              Whether you are a small business or a large company, every
-              successful B2B e-commerce implementation starts with the following
-              10 steps. These are based on the combined expertise and experience
-              of our Project, Sales and Customer Success Managers, here at Sana
-              Commerce. Read on to see exactly what makes a successful
-              e-commerce project plan, and what is the best way to start your
-              e-commerce website.
+            Trippy is owned and managed by Trippy.In.Pvt.Ltd., a leading brand in
+        web designing services and e-commarce soluation.Trippy is counted for
+        its expertise in web soluation and its top ranking business portals. Our
+        invinciable expertise and experiencehas raised our spritsto reach
+        aheadfrom our client's expectation.
             </p>
             <div className="pro-btns">
-              <NavLink to="url.com" className="btn">
+              <a href="https://trippy1998.netlify.app" className="btn">
                 View
-              </NavLink>
-              <NavLink to="url.com" className="btn">
+              </a>
+              <a href="https://github.com/code-with-ayon/Tour" className="btn">
                 Source
-              </NavLink>
+              </a>
             </div>
           </div>
         </div>
+        <div className="project-card">
+          <img src={pro2} alt="image"></img>
+          <h2 className="pro-title">Portfolio</h2>
+          <div className="pro-details">
+            <p>
+            A portfolio is a compilation of academic and professional materials that exemplifies your beliefs, skills, qualifications, education, training, and experiences. It provides insight into your personality and work ethic.
+            </p>
+           
+           <div className="pro-btnss">
+              <a href="https://github.com/code-with-ayon/my-app" className="btn">
+                Source
+              </a>
+            </div>
+          
+          </div>
+        </div>
       </div>
+        </center> 
     </div>
   );
 };
